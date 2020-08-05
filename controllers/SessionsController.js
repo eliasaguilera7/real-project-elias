a2ecb9e96c7e9796c0f861118f3cdae214f3708b
Elias Aguilera <200393648@student.georgianc.on.ca>
2020-08-05 03:53:41 -0400

    fixing bug 4

--- controllers/SessionsController.js
+++ controllers/SessionsController.js
@@ -52,30 +52,3 @@ exports.delete = (req, res) => {
   };
 
 
-/*const User = require('../models/User');
-const passport = require('passport');
-const viewPath = 'sessions';
-
-exports.new = (req, res) => {
-  res.render(`${viewPath}/login`, {
-    pageTitle: 'Login'
-  });
-};
-
-// Step 1: Create an action that will authenticate the user using Passport
-exports.create = (req, res, next) => {
-  passport.authenticate('local', {
-    successRedirect: '/resources',
-    successFlash: 'You are logged in now!.',
-    failureRedirect: '/login',
-    failureFlash: 'Invalid login. Please try again.'
-  })(req, res, next); 
-};
-
-// Step 2: Log the user out
-exports.delete = (req, res) => {
-  req.logout();
-  req.flash('success', 'You were logged now.');
-  res.redirect('/');
-
-};*/
